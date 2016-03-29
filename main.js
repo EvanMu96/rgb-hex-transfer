06d805c966657d5dfee05ddfa23cbe43e1d4af7b
Evan Mu <190567268@qq.com>
2016-03-29 10:40:23 +0800

    实现基本功能

---
+++ main.js
@@ -0,0 +1,8 @@
+function  btnHandle() {
+    var rValue = parseInt(document.getElementById('rValue').value);
+    var gValue = parseInt(document.getElementById('gValue').value);
+    var bValue = parseInt(document.getElementById('bValue').value);
+    var result = ("#"+rValue.toString(16)+gValue.toString(16)+bValue.toString(16));
+    alert(result);
+}
+
