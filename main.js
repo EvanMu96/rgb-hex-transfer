bb9375f53b5d034a95b99a4755dca101973b3c78
Evan Mu <190567268@qq.com>
2016-03-29 10:45:55 +0800

    修改结果字母为大写

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ function  btnHandle() {
     var gValue = parseInt(document.getElementById('gValue').value);
     var bValue = parseInt(document.getElementById('bValue').value);
     var result = ("#"+rValue.toString(16)+gValue.toString(16)+bValue.toString(16));
-    alert(result);
+    alert(result.toUpperCase());
 }
 
